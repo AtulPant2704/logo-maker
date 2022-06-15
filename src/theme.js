0a15d9030de59503c3353b94f43574af1c1da997
AtulPant2704 <atulpant1999@gmail.com>
2022-06-15 22:01:38 +0530

    feat: add input theme

--- src/theme.js
+++ src/theme.js
@@ -10,6 +10,26 @@ export const theme = extendTheme(
         500: "#232323",
       },
     },
+    components: {
+      Input: {
+        variants: {
+          outline: {
+            field: {
+              border: "2px solid white",
+              _hover: {
+                border: "2px solid white",
+              },
+              _focus: {
+                border: "2px solid white",
+              },
+              _active: {
+                border: "2px solid white",
+              },
+            },
+          },
+        },
+      },
+    },
   },
   withDefaultColorScheme({
     colorScheme: "brand",
