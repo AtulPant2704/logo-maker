7c897d04067b02fd618d928f710548dfd83728bd
AtulPant2704 <atulpant1999@gmail.com>
2022-06-16 22:33:19 +0530

    feat: display icon cards according to search

--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -1,6 +1,6 @@
 import { Image, GridItem } from "@chakra-ui/react";
 
-const IconCard = () => {
+const IconCard = ({ images, family_name }) => {
   return (
     <GridItem
       display="flex"
@@ -8,10 +8,10 @@ const IconCard = () => {
       alignItems="center"
       borderTop="1px solid var(--chakra-colors-gray-300)"
       borderLeft="1px solid var(--chakra-colors-gray-300)"
-      _hover={{ bgColor: "bg.500", color: "white" }}
+      _hover={{ cursor: "pointer" }}
       p="16"
     >
-      <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
+      <Image src={images["64"]} alt={family_name} />
     </GridItem>
   );
 };
--- src/pages/LogoIcon.jsx
+++ src/pages/LogoIcon.jsx
@@ -1,12 +1,32 @@
+import { useState, useEffect, useRef } from "react";
 import { Box, Input, Grid } from "@chakra-ui/react";
+import { useLogo } from "context";
+import { getIconsHandler } from "utils";
 import { IconCard } from "components";
 
 const LogoIcon = () => {
+  const { logo, setLogo } = useLogo();
+  const [icons, setIcons] = useState([]);
+  const timerRef = useRef();
+
+  useEffect(() => {
+    if (timerRef.current) {
+      clearTimeout(timerRef.current);
+    }
+    timerRef.current = setTimeout(() => {
+      if (logo.iconName) {
+        getIconsHandler(setIcons, logo.iconToken, logo.iconName);
+      }
+    }, 300);
+  }, [logo.iconName, logo.iconToken]);
+
+  console.log(icons);
+
   return (
     <Box ml={{ base: 0, md: "15rem" }} pt="8">
       <Input
         display="block"
-        placeholder="Search for a logomark"
+        placeholder="Search for a logoicon"
         borderRadius="32"
         bgColor="bg.500"
         paddingX="4"
@@ -16,16 +36,15 @@ const LogoIcon = () => {
         fontWeight="600"
         width="20rem"
         m="auto"
+        value={logo.iconName}
+        onChange={(e) =>
+          setLogo((prev) => ({ ...prev, iconName: e.target.value }))
+        }
       />
       <Grid mt="8" templateColumns="repeat( 2, minmax(100px, 1fr) )">
-        <IconCard />
-        <IconCard />
-        <IconCard />
-        <IconCard />
-        <IconCard />
-        <IconCard />
-        <IconCard />
-        <IconCard />
+        {icons.map((icon) => (
+          <IconCard key={icon.id} {...icon} />
+        ))}
       </Grid>
     </Box>
   );
