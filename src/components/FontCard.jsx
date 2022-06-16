c48fbb7109a8a66a6fabfcabbb3a593c64f44f19
AtulPant2704 <atulpant1999@gmail.com>
2022-06-16 19:30:16 +0530

    refactor: removed last card styling

--- src/components/FontCard.jsx
+++ src/components/FontCard.jsx
@@ -12,12 +12,6 @@ const FontCard = () => {
       borderTop="1px solid var(--chakra-colors-gray-300)"
       borderLeft="1px solid var(--chakra-colors-gray-300)"
       _hover={{ bgColor: "bg.500", color: "white" }}
-      _last={{ borderBottom: "1px solid var(--chakra-colors-gray-300)" }}
-      css={{
-        ":nth-last-child(2)": {
-          borderBottom: "1px solid var(--chakra-colors-gray-300)",
-        },
-      }}
       p="16"
       onClick={() => navigate("/logoicon")}
     >
--- src/components/IconCard.jsx
+++ src/components/IconCard.jsx
@@ -9,12 +9,6 @@ const IconCard = () => {
       borderTop="1px solid var(--chakra-colors-gray-300)"
       borderLeft="1px solid var(--chakra-colors-gray-300)"
       _hover={{ bgColor: "bg.500", color: "white" }}
-      _last={{ borderBottom: "1px solid var(--chakra-colors-gray-300)" }}
-      css={{
-        ":nth-last-child(2)": {
-          borderBottom: "1px solid var(--chakra-colors-gray-300)",
-        },
-      }}
       p="16"
     >
       <Image src="https://bit.ly/dan-abramov" alt="Dan Abramov" />
