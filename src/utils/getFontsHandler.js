ee17a2c28213881bbe940a9df99835ac7ddc80a0
AtulPant2704 <atulpant1999@gmail.com>
2022-06-17 09:51:08 +0530

    fix: get randomised fonts

--- src/utils/getFontsHandler.js
+++ src/utils/getFontsHandler.js
@@ -4,7 +4,14 @@ const getRandomFonts = (fonts) => {
   let fontsArr = [];
   for (let i = 0; i < 30; i++) {
     const randomIndex = Math.floor(Math.random() * 1001);
-    fontsArr.push(fonts[randomIndex]);
+    const findFamily = fontsArr.some(
+      (font) => font["family"] === fonts[randomIndex]["family"]
+    );
+    if (!findFamily) {
+      fontsArr.push(fonts[randomIndex]);
+    } else {
+      i--;
+    }
   }
   return fontsArr;
 };
