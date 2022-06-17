e5fb859d6d01679ce722ceece1cbbfda78b4b823
AtulPant2704 <atulpant1999@gmail.com>
2022-06-17 11:20:07 +0530

    feat: display msgs while network is being made

--- src/pages/LogoIcon.jsx
+++ src/pages/LogoIcon.jsx
@@ -1,5 +1,12 @@
 import { useState, useEffect, useRef } from "react";
-import { Box, Input, Grid } from "@chakra-ui/react";
+import {
+  Box,
+  Input,
+  Grid,
+  Flex,
+  Heading,
+  CircularProgress,
+} from "@chakra-ui/react";
 import { useLogo } from "context";
 import { getIconsHandler } from "utils";
 import { IconCard } from "components";
@@ -7,18 +14,23 @@ import { IconCard } from "components";
 const LogoIcon = () => {
   const { logo, setLogo } = useLogo();
   const [icons, setIcons] = useState([]);
+  const [iconLoader, setIconLoader] = useState(false);
   const timerRef = useRef();
 
   useEffect(() => {
     if (timerRef.current) {
       clearTimeout(timerRef.current);
     }
-    timerRef.current = setTimeout(() => {
+    setIconLoader(true);
+    timerRef.current = setTimeout(async () => {
       if (logo.iconName) {
-        getIconsHandler(setIcons, logo.iconToken, logo.iconName);
+        await getIconsHandler(setIcons, logo.iconToken, logo.iconName);
+        setIconLoader(false);
       }
     }, 300);
   }, [logo.iconName, logo.iconToken]);
+
+  console.log(iconLoader);
 
   return (
     <Box ml={{ base: 0, md: "15rem" }} pt="8">
@@ -39,11 +51,26 @@ const LogoIcon = () => {
           setLogo((prev) => ({ ...prev, iconName: e.target.value }))
         }
       />
+      {icons.length === 0 && logo.iconName === "" ? (
+        <Flex justifyContent="center" alignItems="center" h="80vh">
+          <Heading>Start Searching for an Icon</Heading>
+        </Flex>
+      ) : null}
       <Grid mt="8" templateColumns="repeat( 2, minmax(100px, 1fr) )">
         {icons.map((icon) => (
           <IconCard key={icon.id} {...icon} />
         ))}
       </Grid>
+      {icons.length === 0 && logo.iconName !== "" && !iconLoader ? (
+        <Flex justifyContent="center" alignItems="center" h="80vh">
+          <Heading>No icon found for "{logo.iconName}"</Heading>
+        </Flex>
+      ) : null}
+      {icons.length === 0 && logo.iconName !== "" && iconLoader ? (
+        <Flex justifyContent="center" alignItems="center" h="80vh">
+          <CircularProgress isIndeterminate color="brand.500" />
+        </Flex>
+      ) : null}
     </Box>
   );
 };
